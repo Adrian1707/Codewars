8cdc7007e6cef35848bd16a7447026bf6ee958ac
Adrian Booth <Adrian.booth17@gmail.com>
2015-11-22 08:16:25 +0000

    Add 3 katas

---
+++ stringChunks.js
@@ -0,0 +1,20 @@
+// debug('codewars'.match(/.{1,2}/g));
+
+// function stringChunk(str, n) {
+//  if (n==0){
+//  	return [];
+//  }
+//  else {
+//  var chunks = [];
+// 	for (var i = 0; i < str.length; i += n) {
+// 	    chunks.push(str.substring(i, i + n));
+// 	}
+// }
+// return chunks;
+// }
+
+// debug(stringChunk("sunny day",3));
+
+
+
+// debug(NameMe("Adrian", "Booth"));
---
+++ string_validation(6).rb
@@ -0,0 +1,20 @@
+def validation(string)
+  string.strip!
+  if string =~ /^[a-zA-Z]/ || string.split.count > 1
+      return 'invalid'
+  end
+  if string =~ /^\d/
+    string.to_i + 1
+  elsif string == ""
+    return 1
+  end
+end
+
+
+print validation("7")
+
+
+
+# Test.expect(validation('') == 1)
+# Test.expect(validation(' 123 ') == 124)
+# Test.expect(validation('a') == 'invalid')
