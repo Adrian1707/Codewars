a772c040bf35800a50a9f064d9de0992a9e9d938
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-20 22:26:17 +0000

    summarize_ranges(6).rb

--- range_extract(4).rb
+++ range_extract(4).rb
@@ -1,24 +1,44 @@
+# def solution(list)
+#   final_array = list.each_cons(2).slice_before{|m, n| m + 1 < n}.map{|a| a.map(&:last)}
+#   if list[0] + 1 == list[1]
+#     final_array[0].unshift(list[0])
+#   else
+#     final_array.unshift(list[0])
+#     final_array[0] = [final_array[0]]
+#   end
+#   final_array.map! do |x|
+#     if x.length >= 3
+#       "#{x.first}-#{x.last}"
+#     else
+#       x.join(',')
+#     end
+#   end
+#   final_array.join(',')
+# end
+
+#alternate solution
 def solution(list)
-  final_array = list.each_cons(2).slice_before{|m, n| m + 1 < n}.map{|a| a.map(&:last)}
-  if list[0] + 1 == list[1]
-    final_array[0].unshift(list[0])
-  else
-    final_array.unshift(list[0])
-    final_array[0] = [final_array[0]]
-  end
-  final_array.map! do |x|
-    if x.length >= 3
-      "#{x.first}-#{x.last}"
+  sliced = [[list.shift]]
+  list.map do |x|
+    if sliced[-1][-1] + 1  == x
+      sliced[-1] << x
     else
-      x.join(',')
+      sliced << [x]
     end
   end
-  final_array.join(',')
+  sliced.map! do |x|
+      if x.length >= 3
+        "#{x.first}-#{x.last}"
+      else
+        x.join(',')
+      end
+    end
+    sliced.join(',')
 end
 
 
-# print range_extraction([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# print solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
 
-print solution([-3,-2,-1,2,10,14,15,16,18,19,20])
-
+print solution([1,2,3,4])
+#
 #iterate through array. Check if number is one less than the next one
---
+++ summarize_ranges(6).rb
@@ -0,0 +1,29 @@
+def summary_ranges(nums)
+  if nums.empty?
+    return []
+  end
+  sliced = [[nums.shift]]
+  nums.map do |x|
+    if sliced[-1][-1] + 1  == x || sliced[-1][-1] == x
+      sliced[-1] << x
+    else
+      sliced << [x]
+    end
+  end
+  sliced[0].uniq!
+  sliced.map! do |x|
+      if x.length >= 2
+        sliced.uniq!
+        "#{x.first}->#{x.last}"
+      else
+        x.join(',')
+      end
+    end
+end
+
+# print summary_ranges([0, 1, 2, 5, 6, 9])
+# print summary_ranges([0, 1, 2, 3, 3, 3, 4, 5, 6, 7])
+# print summary_ranges([0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10])
+# print summary_ranges([-2,0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10, 12])
+# print summary_ranges([1,1,1,1])
+print summary_ranges([])
