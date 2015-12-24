fdcb5fc0a09d0911d1619a9510f7048de112b275
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-24 15:19:00 +0000

    findFactors(7).js

---
+++ findFactors(7).js
@@ -0,0 +1,22 @@
+function factors(x){
+	var numberArray = [];
+	factorArray = [];
+	if(x<0 || x % 1 !== 0 || x.NaN == true || x == 0){
+		return -1;
+	}
+	else{
+		for(var i = 0; i<x+1; i++){
+			numberArray.push(i);
+	}
+	}
+	for(var i = 0; i<numberArray.length; i++){
+		if(x % numberArray[i] == 0){
+			factorArray.push(numberArray[i])
+		}
+	}
+	return factorArray.reverse();
+}
+
+debug(factors("dn"))
+// debug(factors(10))
+
