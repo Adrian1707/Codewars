e20cd54e4ba9c9db8a45ebbc7b88220b430e46aa
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-24 11:21:15 +0000

    Complete characterCount(7).js

---
+++ characterCount(7).js
@@ -0,0 +1,39 @@
+function characterCounter(string){
+	var sorted_array = string.toLowerCase().split('').sort();
+	var count_array = [];
+	var count = 0;
+	for(var i = 0; i < sorted_array.length; i++){
+		if(sorted_array[i]==sorted_array[i+1]){
+			count++;
+		}
+		else{
+			count_array.push(count+1);
+			count = 0;
+		}
+	}	
+	var n = [];
+	for(var i = 0; i < count_array.length; i++) 
+	{
+		if (n.indexOf(count_array[i]) == -1) n.push(count_array[i]);
+	}
+	if(n.length == 1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+debug(characterCounter("ababab"))
+//if the number of zeros is higher then the maximum number in the array, it's invalid
+// debug(characterCounter(""))
+// debug(characterCounter("7huqoxigt2p*gvk@:uaq4%bg1ma77hhuqooxxiitt22pp**vvkk@@::a44%bb11mm"))
+// debug(characterCounter("abcab1>cabcabcabcabcabc!38lkbeyq;[q"))
+// valid if each word has exact same number of occurances 
+
+// logic: If string.uniq.length == string.length.uniq x 2 || string.uniq.length == string.lengh, it's a valid word. Why?
+//because 
+
+// "abcabc".uniq == "abc" Valid because 
+// "123abc!".uniq == "123abc!" Valid 
+// "abcabcd".uniq = "abcd" Invalid 
+// "abab!".uniq = "ab!" Invalid because "ab!".length x2 != string.length 
