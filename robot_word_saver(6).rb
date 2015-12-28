f9bb818aab281426cc05564c309d969f9c7f6238
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-28 23:46:41 +0000

    code passes all random tests

--- robot_word_saver(6).rb
+++ robot_word_saver(6).rb
@@ -7,8 +7,9 @@ class Robot
   end
 
   def learn_word(word)
-    return "I already know the word #{word}" if has_word?(word) || @memorised_words.include?(word.downcase)
+    return "I already know the word #{word}" if has_word?(word) || @memorised_words.include?(word.downcase) || @memorised_words.join('').split('').include?(word.downcase)
     return "I do not understand the input" if word != word[/[a-zA-Z]+/] || word.match(/\s/)
+    word.downcase! unless word == word.upcase
     @memorised_words << word
     return "Thank you for teaching me #{word}"
   end
@@ -21,11 +22,19 @@ end
 
 vicky = Robot.new
 
-puts vicky.learn_word("hello")
-puts vicky.learn_word("world")
-puts vicky.learn_word("World")
-puts vicky.learn_word("WORLD")
-puts vicky.learn_word("a whole bunch of words")
-puts vicky.learn_word(".not")
+# puts vicky.learn_word("hello")
+# puts vicky.learn_word("world")
+# puts vicky.learn_word("World")
+# puts vicky.learn_word("WORLD")
+# puts vicky.learn_word("a whole bunch of words")
+# puts vicky.learn_word(".not")
+# puts vicky.learn_word("moriBiTo")
+# puts vicky.learn_word("MorIbiTo")
+puts vicky.learn_word("for")
+puts vicky.learn_word("BIg")
+puts vicky.learn_word("dO")
+puts vicky.learn_word("i")
+puts vicky.learn_word("kno?w")
+# print vicky.memorised_words.join('').split('')
 
 # vicky.learn_word('hey')
