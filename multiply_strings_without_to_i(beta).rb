c9ca4bf4a4ac4446d395911021051a42bfcf2963
Adrian Booth <Adrian.booth17@gmail.com>
2016-01-04 16:15:32 +0000

    Multiply strings problem

---
+++ multiply_strings_without_to_i(beta).rb
@@ -0,0 +1,8 @@
+def multiply(a,b)
+  a_num = Integer(a) rescue 0
+  b_num = Integer(b) rescue 0
+  multiple = a_num * b_num
+  String(multiply) rescue 0
+end
+
+print multiply("12", "2")
--- permutations_problem(6).rb
+++ permutations_problem(6).rb
@@ -1,22 +1,25 @@
-def combinations(array)
+def ssc_forperm(arr)
   arrays = []
   permutations = []
   sums = []
-  array.permutation{|x| arrays << x}
-  arrays
+  arr.permutation{|x| arrays << x}
   arrays.map.with_index do |x,i|
     x.each_with_index do |y,i|
         permutations << y * (i+1)
     end
   end
-  permutations.each_slice(3){|x| sums << x}
+  permutations.each_slice(arr.length){|x| sums << x}
   sums.map! do |x|
     x.inject(:+)
   end
+  sums.uniq!
   sum = sums.inject(:+)
   maximum = sums.max
   minimum = sums.min
   length = sums.length
-end 
+end
 
-print combinations([6,12,-1])
+puts ssc_forperm([6,12,-1])
+
+puts ssc_forperm([4, 25, 5, 11, 4])
+puts ssc_forperm([1, 1, 1, 11, 2])
