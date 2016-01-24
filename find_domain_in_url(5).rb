c75ff549dfca15ae3a9eef621a4171804a8f5dbe
Adrian Booth <Adrian.booth17@gmail.com>
2016-01-24 15:22:06 +0000

    remove_smallest(7).py

--- find_domain_in_url(5).rb
+++ find_domain_in_url(5).rb
@@ -13,6 +13,7 @@ def split_domain
   domain_name = domain.split(".").first
 end
 
+
 print domain_name("http://github.com/carbonfive/raygun")
 
 print domain_name("https://www.cnet.com")
---
+++ remove_smallest(7).py
@@ -0,0 +1,9 @@
+def remove_smallest(numbers):
+    if numbers == []:
+        return []
+    numbers.remove(min(numbers))
+    return numbers
+
+print remove_smallest([1,2,3,4])
+print remove_smallest([5,3,2,1,4])
+print remove_smallest([])
