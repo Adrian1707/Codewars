614e60a9944c817dd1ee222dd063f4f52710136c
Adrian Booth <Adrian.booth17@gmail.com>
2016-02-04 11:47:12 +0000

    zeroFill(6).js

---
+++ javascript/zeroFill(6).js
@@ -0,0 +1,15 @@
+function zeroFill(number, size) {
+  number = Math.abs(~~number); 
+  var numberLength = number.toString().length;
+  var numArray = [];
+  for(var i = 0; i<size;i++){
+  	numArray.push(0);
+  } 
+  for(var i = 0;i<numberLength;i++){
+  	numArray.shift()
+  }
+  numArray.push(number);
+  return numArray.join('');	
+}
+
+debug(zeroFill(11,11));
